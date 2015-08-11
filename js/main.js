baac5e18e6f7d689c3bbf8013361299867eab1aa
amerriman <amm434@gmail.com>
2015-08-11 16:23:25 -0600

    functional

--- js/main.js
+++ js/main.js
@@ -32,8 +32,28 @@ Game.prototype.organize = function(libraries){
   }
 };
 
-
-
+function findLibrary(tempValue){
+  if(tempLibrary === "videoGames"){
+    //append video games to the DOM
+    $("#all-games").append('<h4>Video Games</h4>');
+     //loop through the GameLibrary.games array and append position[0].title, then position[1].title
+    for (var i = 0; i < videoGames.games.length; i++) {
+      $("#all-games").append('<p> Title:' + videoGames.games[i].title + '</p>');
+    }
+  }
+  else if(tempLibrary === "boardGames"){
+    $("#all-games").append('<h4>Board Games</h4>');
+    for (var j = 0; j < boardGames.games.length; j++) {
+      $("#all-games").append('<p> Title:' + boardGames.games[j].title + '</p>');
+    }
+  }
+  else{
+    $("#all-games").append('<h4>Drinking Games</h4>');
+    for (var k = 0; k < drinkingGames.games.length; k++) {
+      $("#all-games").append('<p> Title:' + drinkingGames.games[k].title + '</p>');
+    }
+  }
+}
 
 //GAME LIBRARY STUFF
 var videoGames = new GameLibrary("Video Games");
@@ -47,10 +67,11 @@ var marioCart = new Game("Mario Cart", "Video Games");
 
 //console.log(marioCart.organize(libraries));
 var temp = new Game();
+var tempLibrary;
 
 $(document).on('ready', function() {
 
-  $('form').on('submit', function(event){
+  $('form').not("#show-library").on('submit', function(event){
     event.preventDefault();
     var titleInput = $('#gameinput').val();
     var gameType = $('#gametypes').val();
@@ -58,13 +79,22 @@ $(document).on('ready', function() {
     var game = new Game(titleInput, gameType);
     temp = game;
     temp.organize(libraries);
-    $(".form-input").val("");
+    $(".game-input").val("");
+    $("#all-games").html("");
+
+    $("#new-games").append('<p> Game Created - Title: ' + temp.title + ' Type: ' + temp.type + '</p>');
+
+  });
+
+  $("#show-library").on('submit', function(event){
+    event.preventDefault();
+    var libraryType = $('#librarytypes').val();
+    tempLibrary = libraryType;
+    //console.log("woot");
+    $("#all-games").html("");
+    findLibrary(tempLibrary);
 
 
-  //$('body').append(titleInput + "  " + gType);
-  $('body').append('<p> Game Created - Title: ' + temp.title + ' Type: ' + temp.type + '</p>');
-  //$('body').append('<p>lalala '+ temp.title' </p>');
-    //console.log(titleInput);
   });
 
 });
